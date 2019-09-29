57b305cefa88a7c5135478533cefe40955102c34
Jenpar1674 <51331242+Jenpar1674@users.noreply.github.com>
2019-09-29 15:22:01 -0500

    Initial commit

---
+++ client/src/Components/UnhandledError.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <div className="bounds">
+    <h2; className="error--title">Unexpected (</h2>
+    <p>500;; Internal; Server; Error</p>
+  </div>;
+)
